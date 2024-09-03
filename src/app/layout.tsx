426b32bcc78745f3671cfb1d8f439dce8905fe37
남희정 (Nami) <78931643+havenothorn@users.noreply.github.com>
2024-09-04 08:49:45 +0900

    view: add global style (#9)
    * view: add global style
    * view: logoImg
    * view: modify header style
    
    close #8

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { GeistMono } from "geist/font/mono";
+import * as styles from "./globals.css";
 
 export const metadata: Metadata = {
   title: "Nami-Socket",
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={GeistMono.className}>{children}</body>
+      <body className={[GeistMono.className, styles.body].join(" ")}>
+        {children}
+      </body>
     </html>
   );
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
-import { header, logo, menu, nav } from "./styles.css";
+import { header, logo, logoImg, menu, nav } from "./styles.css";
 
 const Header = () => {
   return (
     <header className={header}>
       <div className={logo}>
-        <img src="/logo.svg" alt="logo" />
+        <img className={logoImg} src="/logo.png" alt="logo" />
         <h1>Nami.Socket()</h1>
       </div>
       <nav className={nav}>
